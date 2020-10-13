ab26de7889a4473febc0406e8b8e168d24795e0c
Emilio Depena <emiliosdep@gmail.com>
2020-10-13 17:39:45 -0400

    Initial commit

---
+++ src/com/emisdep/Die.java
@@ -0,0 +1,29 @@
+package com.emisdep;
+
+import java.util.Random;
+
+public class Die {
+
+    private int value;
+    private int sides;
+
+    public Die() {
+        value = 1;
+    }
+
+    public Die(int sides, int value) {
+        this.sides = sides;
+        this.value = value;
+    }
+
+    public void roll(Random rand) {
+        value = rand.nextInt(sides) + 1;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
+
+
+
